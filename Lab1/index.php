b9935c2da0e8ca9c073c62256bf7daf9f953a65b
Jaegar Sarauer <jaegars@hotmail.com>
2016-01-12 20:18:45 -0800

    Encapsulate logic in object.

--- Lab1/index.php
+++ Lab1/index.php
@@ -2,25 +2,36 @@
 $board_string = $_GET['board'];
 $squares = str_split($board_string); //index 0 = top left, index 8 = bottom right
 
-
-$is_winner = testWinner($squares);
+$game = new Game($squares);
+$is_winner = $game->testWinner();
 echo ($is_winner == null) ? "No winner" : $is_winner . " wins!";
-//takes in an array of 3x3 of a tic-tac-toe board and computes a winner.
-//Returns null character on no winner, 'X' if X player wins, and 'O' if O player wins.
-function testWinner($squares) {
+
     
-    for ($i = 0; $i < count($squares) / 3; $i++) {
-        if ($squares[$i] != '-' && $squares[($i * 3) + 1] == $squares[$i * 3] && $squares[($i * 3) + 2] == $squares[$i * 3]) {
-            return $squares[$i * 3];
-        } else if ($squares[$i] != '-' && $squares[$i + 3] == $squares[0] && $squares[$i + 6] == $squares[$i]) {
-            return $squares[$i];
+    
+class Game {
+    var $squares;
+    
+    function __construct($squares_in) {
+        $this->squares = $squares_in;
+    }
+    
+    //takes in an array of 3x3 of a tic-tac-toe board and computes a winner.
+    //Returns null character on no winner, 'X' if X player wins, and 'O' if O player wins.
+    function testWinner() {
+    
+    for ($i = 0; $i < count($this->squares) / 3; $i++) {
+        if ($this->squares[$i] != '-' && $this->squares[($i * 3) + 1] == $this->squares[$i * 3] && $this->squares[($i * 3) + 2] == $this->squares[$i * 3]) {
+            return $this->squares[$i * 3];
+        } else if ($this->squares[$i] != '-' && $this->squares[$i + 3] == $this->squares[0] && $this->squares[$i + 6] == $this->squares[$i]) {
+            return $this->squares[$i];
         }
     }
 
-    if (($squares[4] != '-') && (($squares[0] == $squares[4] && $squares[8] == $squares[4]) || ($squares[2] == $squares[4] && $squares[6] == $squares[4]))) {
-        return $squares[4];
+    if (($this->squares[4] != '-') && (($this->squares[0] == $this->squares[4] && $this->squares[8] == $this->squares[4]) || ($this->squares[2] == $this->squares[4] && $this->squares[6] == $this->squares[4]))) {
+        return $this->squares[4];
     }
     
     return null;
     }
+}
 ?>
