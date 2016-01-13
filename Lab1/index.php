ed8322ada03c6eabee43f479f2b26bd7372b6f43
Jaegar Sarauer <jaegars@hotmail.com>
2016-01-12 16:51:11 -0800

    you owe me money

--- Lab1/index.php
+++ Lab1/index.php
@@ -5,4 +5,10 @@ $temp = "forehead";
 echo '<br/>I am a ' . $temp;
 $temp = 99;
 echo '<br/>my level is ' . $temp;
+
+
+$hoursworked = 100;
+$rate = 66;
+$total = $hoursworked * $rate;
+echo "</br>you owe me $" . $total;
 ?>
